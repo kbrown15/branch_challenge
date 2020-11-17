c2bd3df756b2f2e75c6215205cd9d62570dfc9da
kbrown2 <kirsten.brown@merrillcorp.com>
2020-11-17 09:49:44 -0600

    initial structure

---
+++ src/main/java/com/example/branch_challenge/BranchChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.branch_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/branch_challenge/BranchChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.branch_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

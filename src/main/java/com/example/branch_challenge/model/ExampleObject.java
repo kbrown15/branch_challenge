120c2604bb7f975e0fc4a132929101666b0bd640
kbrown2 <kirsten.brown@merrillcorp.com>
2020-11-17 11:55:26 -0600

    cleanup and jockson mapping

--- src/main/java/com/example/branch_challenge/model/ExampleObject.java
+++ src/main/java/com/example/branch_challenge/model/ExampleObject.java
@@ -1,88 +1,88 @@
 package com.example.branch_challenge.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.net.URL;
 import java.util.Date;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExampleObject {
     public String user_name;
     public String display_name;
     public URL avatar;
-    public String geo_Location;
+    public String geo_location;
     //TODO: make email validator
     public String email;
     public  URL url;
     public Date created_at;
     public ExampleRepo[] repos;
 
+    @JsonProperty("login")
     public String getUser_name() {
         return user_name;
     }
 
-    public ExampleObject setUser_name(String user_name) {
+    public void setUser_name(String user_name) {
         this.user_name = user_name;
-        return this;
     }
 
+    @JsonProperty("name")
     public String getDisplay_name() {
         return display_name;
     }
 
-    public ExampleObject setDisplay_name(String display_name) {
+    public void setDisplay_name(String display_name) {
         this.display_name = display_name;
-        return this;
     }
 
+    @JsonProperty("avatar_url")
     public URL getAvatar() {
         return avatar;
     }
 
-    public ExampleObject setAvatar(URL avatar) {
+    public void setAvatar(URL avatar) {
         this.avatar = avatar;
-        return this;
     }
 
-    public String getGeo_Location() {
-        return geo_Location;
+    @JsonProperty("location")
+    public String getGeo_location() {
+        return geo_location;
     }
 
-    public ExampleObject setGeo_Location(String geo_Location) {
-        this.geo_Location = geo_Location;
-        return this;
+    public void setGeo_location(String geo_location) {
+        this.geo_location = geo_location;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public ExampleObject setEmail(String email) {
+    public void setEmail(String email) {
         this.email = email;
-        return this;
     }
 
     public URL getUrl() {
         return url;
     }
 
-    public ExampleObject setUrl(URL url) {
+    public void setUrl(URL url) {
         this.url = url;
-        return this;
     }
 
     public Date getCreated_at() {
         return created_at;
     }
 
-    public ExampleObject setCreated_at(Date created_at) {
+    public void setCreated_at(Date created_at) {
         this.created_at = created_at;
-        return this;
     }
 
     public ExampleRepo[] getRepos() {
         return repos;
     }
 
-    public ExampleObject setRepos(ExampleRepo[] repos) {
+    public void setRepos(ExampleRepo[] repos) {
         this.repos = repos;
-        return this;
     }
 }

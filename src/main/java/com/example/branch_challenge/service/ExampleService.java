0f6d183d95418aed655737280113c5058a19a4bd
kbrown2 <kirsten.brown@merrillcorp.com>
2020-11-17 11:55:56 -0600

    inject dependency

--- src/main/java/com/example/branch_challenge/service/ExampleService.java
+++ src/main/java/com/example/branch_challenge/service/ExampleService.java
@@ -2,11 +2,17 @@ package com.example.branch_challenge.service;
 
 import com.example.branch_challenge.delegate.ExampleDelegate;
 import com.example.branch_challenge.model.ExampleObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExampleService {
     private ExampleDelegate delegate;
+
+    @Autowired
+    public ExampleService(ExampleDelegate delegate) {
+        this.delegate = delegate;
+    }
 
     public ExampleObject getAPIResponse(String username) {
         ExampleObject exampleObject = delegate.getMainObject(username);
